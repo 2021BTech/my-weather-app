a1de5abb3f421f62a6a0e6b3f27df214d1202bfe
2021BTech <daffydanni@gmail.com>
2023-06-21 14:21:55 +0100

    weather got me this way

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "395eda34e7mshf7568849cf94820p178ed4jsn2fa57d5c059e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9a81711c5daaf6c09899594d99e92f6b";
